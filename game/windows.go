e2bc1c17975fe367267c9d10117ddcbc86ef5aea
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-28 23:33:09 +0000

    Create windows.go

---
+++ game/windows.go
@@ -0,0 +1,18 @@
+package game
+
+import (
+	"github.com/amyadzuki/amystuff/styles"
+
+	"github.com/g3n/engine/gui"
+)
+
+func (game *Game) AddWindowInventory() {
+	game.WindowInventory = gui.NewWindow(DflWidth, DflHeight)
+	game.WindowInventory.SetTitle("Inventory") // TODO: translate
+	game.WindowInventory.SetPosition(60, 60)
+	// Resizable windows are currently buggy.
+	// game.WindowInventory.SetResizable(gui.ResizeAll)
+	game.WindowInventory.SetLayout(gui.NewFillLayout(true, true))
+	game.WindowInventory.SetColor4(&styles.AmyDarkWindowContent)
+	game.Gui.Add(game.WindowInventory)
+}
