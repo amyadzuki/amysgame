ea8c209e9072aff3620cb569da2be230d5f96e91
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-28 23:31:28 +0000

    Create widgets.go

---
+++ game/widgets.go
@@ -0,0 +1,139 @@
+package game
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"reflect"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/amyadzuki/amygolib/str"
+
+	"github.com/amyadzuki/amystuff/logs"
+	"github.com/amyadzuki/amystuff/styles"
+	"github.com/amyadzuki/amystuff/widget"
+
+	//	"github.com/g3n/engine/audio"
+	"github.com/g3n/engine/camera"
+	"github.com/g3n/engine/core"
+	"github.com/g3n/engine/gls"
+	"github.com/g3n/engine/gui"
+	"github.com/g3n/engine/light"
+	"github.com/g3n/engine/math32"
+	"github.com/g3n/engine/renderer"
+	"github.com/g3n/engine/window"
+
+	"github.com/golang-ui/nuklear/nk"
+
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+func (game *Game) AddWidgetCharaChanger(label string) {
+	if game.DockTop == nil {
+		game.AddDockTop()
+	}
+	game.LabelCharaChangerBlank = label
+	game.WidgetCharaChanger = gui.NewButton(label)
+	game.WidgetCharaChanger.SetLayoutParams(&gui.DockLayoutParams{gui.DockLeft})
+	game.WidgetCharaChanger.Subscribe(gui.OnClick, func(name string, ev interface{}) {
+		//
+	})
+	game.addDockSize(game.DockTop, game.WidgetCharaChanger)
+	game.DockTop.Add(game.WidgetCharaChanger)
+}
+
+func (game *Game) AddWidgetClose(label string) {
+	if game.DockTop == nil {
+		game.AddDockTop()
+	}
+	game.WidgetClose = gui.NewButton(label)
+	game.WidgetClose.SetLayoutParams(&gui.DockLayoutParams{gui.DockRight})
+	game.WidgetClose.SetStyles(&styles.AmyDarkCloseButton)
+	game.WidgetClose.Subscribe(gui.OnClick, func(name string, ev interface{}) {
+		game.WidgetClose.SetStyles(&styles.AmyDarkClosingButton)
+		if game.SoftQuit() > 0 {
+			game.Quit()
+		}
+		go func() {
+			time.Sleep(time.Second)
+			game.AskQuit--
+			if game.AskQuit < 0 {
+				game.AskQuit = 0
+			}
+			game.WidgetClose.SetStyles(&styles.AmyDarkCloseButton)
+		}()
+	})
+	game.addDockSize(game.DockTop, game.WidgetClose)
+	game.DockTop.Add(game.WidgetClose)
+}
+
+func (game *Game) AddWidgetFps() {
+	game.AddWidgetPerformance(&game.WidgetFps, 999999, " fps  ")
+}
+
+func (game *Game) AddWidgetFullScreen(labelFullScreen, labelWindow string) {
+	if game.DockTop == nil {
+		game.AddDockTop()
+	}
+	game.LabelFullScreen = labelFullScreen
+	game.LabelWindow = labelWindow
+	label := labelFullScreen
+	if game.FullScreen() {
+		label = labelWindow
+	}
+	game.WidgetFullScreen = gui.NewButton(label)
+	game.WidgetFullScreen.SetLayoutParams(&gui.DockLayoutParams{gui.DockRight})
+	game.WidgetFullScreen.Subscribe(gui.OnClick, func(name string, ev interface{}) {
+		game.ToggleFullScreen()
+	})
+	game.addDockSize(game.DockTop, game.WidgetFullScreen)
+	game.DockTop.Add(game.WidgetFullScreen)
+}
+
+func (game *Game) AddWidgetHelp(label string) {
+	if game.DockTop == nil {
+		game.AddDockTop()
+	}
+	game.WidgetHelp = gui.NewButton(label)
+	game.WidgetHelp.SetLayoutParams(&gui.DockLayoutParams{gui.DockRight})
+	game.WidgetHelp.Subscribe(gui.OnClick, func(name string, ev interface{}) {
+		game.WantHelp = !game.WantHelp
+	})
+	game.addDockSize(game.DockTop, game.WidgetHelp)
+	game.DockTop.Add(game.WidgetHelp)
+}
+
+func (game *Game) AddWidgetHint(label string) {
+	if game.DockTop == nil {
+		game.AddDockTop()
+	}
+	game.WidgetHint.Init(label)
+	game.WidgetHint.Panel.SetLayoutParams(&gui.DockLayoutParams{gui.DockLeft})
+	game.DockTop.Add(game.WidgetHint.Panel)
+}
+
+func (game *Game) AddWidgetIconify(label string) {
+	if game.DockTop == nil {
+		game.AddDockTop()
+	}
+	game.WidgetIconify = gui.NewButton(label)
+	game.WidgetIconify.SetLayoutParams(&gui.DockLayoutParams{gui.DockRight})
+	game.WidgetIconify.Subscribe(gui.OnClick, func(name string, ev interface{}) {
+		// TODO
+	})
+	game.addDockSize(game.DockTop, game.WidgetIconify)
+	game.DockTop.Add(game.WidgetIconify)
+}
+
+func (game *Game) AddWidgetPerformance(w *widget.Performance, large int, label string) {
+	w.Init(large, label)
+	w.Outer.SetLayoutParams(&gui.DockLayoutParams{gui.DockRight})
+	game.DockTop.Add(w.Outer)
+}
+
+func (game *Game) AddWidgetPing() {
+	game.AddWidgetPerformance(&game.WidgetPing, 999999, " ms  ")
+}
