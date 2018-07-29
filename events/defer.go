e9b021c4bb5ffecca214b16b01bd10d8642407df
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-29 01:04:32 +0000

    Update defer.go

--- events/defer.go
+++ events/defer.go
@@ -1,11 +1,7 @@
 package events
 
-func defer(name, evname string, event interface{}, fn, fallback func(string, string, interface{})) {
+func defer(name, evname string, event interface{}, fn func(string, string, interface{})) {
 	if fn != nil {
-		fn(name, evname, event)
-		return
-	}
-	if fallback != nil {
 		fn(name, evname, event)
 		return
 	}
