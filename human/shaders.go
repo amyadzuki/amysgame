d145f5964310b887cd597e419f27d4ed3e577ad2
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-29 12:45:01 +0000

    Update shaders.go

--- human/shaders.go
+++ human/shaders.go
@@ -20,9 +20,14 @@ in vec2 vTexcoord;
 out vec4 fColor;
 void main() {
 #if MAT_TEXTURES>0
-	fColor = vec4(texture(MatTexture[0], vTexcoord));
+	fColor = texture(MatTexture[0], vTexcoord);
 #if MAT_TEXTURES>1
-	// TODO: underwear
+	vec4 uwfc, uw;
+	uwfc = texture(MatTexture[1], vTexcoord);
+	uw = mix(vec4(0, 0, 0, 0), vec4(1, 1, 1, 1), uwfc.r)
+	uw = mix(uw, vec4(0.875, 0.875, 0.875, 0.5), uwfc.g)
+	uw = mix(uw, vec4(0xff/255.0, 0xb6/255.0, 0xc1/255.0, 1), uwfc.b)
+	fColor = mix(fColor, uw, uwfc.a)
 #endif
 #else
 	fColor = vec4(1, 0, 1, 1);
