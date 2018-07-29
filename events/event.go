8061c1c78846d4c88cccf2651198c1f2aa15d214
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-29 01:26:02 +0000

    Update event.go

--- events/event.go
+++ events/event.go
@@ -4,19 +4,19 @@ var Event map[EventId]Mapping
 
 func init() {
 	Event = make(map[EventId]Mapping)
-	Event[Inventory] = Mapping{"Inventory", OnInventory}
-	Event[ToggleFullscreen] = Mapping{"Toggle Fullscreen", OnToggleFullscreen}
+	Event[ToggleFullScreen] = Mapping{"Toggle FullScreen", OnToggleFullScreen}
+	Event[ToggleInventory] = Mapping{"Inventory", OnToggleInventory}
 }
 
 type EventId int
 const (
 	nothing EventId = iota
-	Inventory
-	ToggleFullscreen
+	ToggleFullScreen
+	ToggleInventory
 )
 
 var (
 	onNothing func(string, string, event interface{})
+	OnToggleFullScreen
 	OnInventory
-	OnToggleFullscreen
 )
