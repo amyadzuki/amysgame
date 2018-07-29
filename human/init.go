a5f7707be5444b837d57c2f4e4aac6c14d6f7714
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-29 12:25:23 +0000

    Update init.go

--- human/init.go
+++ human/init.go
@@ -4,9 +4,26 @@ import (
 	"github.com/g3n/engine/renderer"
 )
 
+var (
+	NakedSkin *texture.Texture2D
+	Underwear *texture.Texture2D
+)
+
 func Init(rend *renderer.Renderer) {
 	rend.AddShader("HumanSkinVs", HumanSkinVs)
 	rend.AddShader("HumanSkinFs", HumanSkinFs)
 	rend.AddProgram("HumanSkin", "HumanSkinVs", "HumanSkinFs")
 	// TODO: do these return error codes?
+
+	NakedSkin = Load("00-naked.png")
+	Underwear = Load("10-under.png")
+}
+
+func Load(path string) *texture.Texture2D {
+	tex, err := texture.NewTexture2DFromImage(path)
+	if err != nil {
+		panic(err)
+	}
+	tex.SetFlipY(false)
+	return tex
 }
