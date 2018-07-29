7f3e8e267083f0335059d483e558f2d84293e34d
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-29 04:55:42 +0000

    Update event.go

--- events/event.go
+++ events/event.go
@@ -5,9 +5,6 @@ var EventName map[Id]string
 
 func init() {
 	Event = make(map[Id]Mapping)
-	Event[ToggleFullScreen] = Mapping{"Toggle FullScreen", nil}
-	Event[ToggleInventory] = Mapping{"Inventory", nil}
-
 	EventName = make(map[Id]string)
 	EventName[ToggleFullScreen] = "Toggle FullScreen"
 	EventName[ToggleInventory] = "Inventory"
