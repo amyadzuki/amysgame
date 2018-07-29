d3ee23958a7f4770492a4a3cc12f88ded6656a61
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-29 01:28:55 +0000

    Update keycode.go

--- events/keycode.go
+++ events/keycode.go
@@ -8,13 +8,22 @@ var Keycode map[int]EventId
 
 func init() {
 	Keycode = make(map[int]EventId)
-	Keycode[window.KeyI] = Inventory
-	Keycode[window.KeyF11] = ToggleFullscreen
+	Keycode[window.KeyI] = ToggleInventory
+	Keycode[window.KeyF11] = ToggleFullScreen
 }
 
 func OnKeyboardKey(evname string, event interface{}) {
 	ev, ok := event.(*window.KeyEvent)
 	if !ok {
+		return
+	}
+	var down bool
+	switch ev.Action {
+	case window.Press:
+		down = true
+	case window.Release:
+		down = false
+	default:
 		return
 	}
 	kei, ok := Keycode[ev.Keycode]
@@ -25,5 +34,5 @@ func OnKeyboardKey(evname string, event interface{}) {
 	if !ok {
 		return
 	}
-	m.Defer(evname, event)
+	m.Defer(evname, event, down)
 }
