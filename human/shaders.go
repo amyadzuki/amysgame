597445e1ab572f140177565c5cd5db9047037c9f
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-29 12:22:24 +0000

    Update shaders.go

--- human/shaders.go
+++ human/shaders.go
@@ -1,18 +1,31 @@
 package human
 
-import (
-	"github.com/g3n/engine/renderer"
-)
-
-func InitShaders(rend *renderer.Renderer) {
-	rend.AddShader("HumanVs", HumanVs)
-	rend.AddShader("HumanFs", HumanFs)
-	rend.AddProgram("Human", "HumanVs", "HumanFs")
-	// TODO: do these return error codes?
+var HumanSkinVs = `
+#include <attributes>
+// blank line required by preprocessor
+uniform mat4 MVP;
+out vec2 vTexcoord;
+void main() {
+	vec4 position;
+	position = vec4(VertexPosition.xyz, 1);
+	gl_Position = MVP * position;
+	vTexcoord = VertexTexcoord.xy;
 }
-
-var HumanVs = `
 `
 
-var HumanFs = `
+var HumanSkinFs = `
+#include <material>
+// blank line required by preprocessor
+in vec2 vTexcoord;
+out vec4 fColor;
+void main() {
+#if MAT_TEXTURES>0
+	fColor = vec4(texture(MatTexture[0], vTexcoord));
+#if MAT_TEXTURES>1
+	// TODO: underwear
+#endif
+#else
+	fColor = vec4(1, 0, 1, 1);
+#endif
+}
 `
