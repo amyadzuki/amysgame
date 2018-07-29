d7b811196b945e6c238cceb5fd98b4a3aa94723d
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-29 12:12:02 +0000

    Update human.go

--- human/human.go
+++ human/human.go
@@ -6,13 +6,14 @@ import (
 
 	"github.com/g3n/engine/core"
 	"github.com/g3n/engine/loader/obj"
+	"github.com/g3n/engine/material"
 )
 
 type Human struct {
 	Node *core.Node
 
-	Eyes *obj.Object
-	Skin *obj.Object
+	Eyes *graphic.Mesh
+	Skin *graphic.Mesh
 
 	heightToEye float64
 }
@@ -27,17 +28,15 @@ func (human *Human) HeightToEye() float64 {
 	return human.heightToEye
 }
 
-func (human *Human) Init(dec *obj.Decoder) error {
+func (human *Human) Init(dec *obj.Decoder) (err error) {
 	human.Node = core.NewNode()
 	for idx := 0; idx < len(dec.Objects); idx++ {
-		mesh, err := dec.NewMesh(&dec.Objects[idx])
-		if err != nil {
-			return err
-		}
+		var mesh *graphic.Mesh
 		name := dec.Objects[idx].Name
 		switch {
 		case strings.HasSuffix(name, "-highpolyeyes"):
 			human.Eyes = &dec.Objects[idx]
+			mesh, err = dec.NewMesh(human.Eyes)
 			index := dec.Objects[idx].Faces[0].Vertices[0]
 			_ = index // TODO
 			// human.heightToEye = TODO:
@@ -45,10 +44,27 @@ func (human *Human) Init(dec *obj.Decoder) error {
 			fallthrough
 		case strings.HasSuffix(name, "-male_generic"):
 			human.Skin = &dec.Objects[idx]
+			mesh, err = NewMeshSkin(dec, human.Skin)
 		default:
 			fmt.Printf("human.Init: Name: \"%s\"\n", name)
+		}
+		if err != nil {
+			return
 		}
 		human.Node.Add(mesh)
 	}
 	return nil
+}
+
+var NewMeshSkin (dec *obj.Decoder, object *obj.Object) (*graphic.Mesh, error) {
+	geom, err := dec.NewGeometry(object)
+	if err != nil {
+		return nil, err
+	}
+	mat := new(material.Standard)
+	mat.Init("HumanSkin", math32.NewColor("magenta"))
+	mat.AddTexture(NakedSkin)
+	mat.AddTexture(Underwear)
+	mesh := graphic.NewMesh(geom, mat)
+	return mesh, nil
 }
