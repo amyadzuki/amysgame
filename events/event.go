8d7ff006fe9826eca56b1a7cffdd182af6ec4e6a
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-29 01:17:08 +0000

    Update event.go

--- events/event.go
+++ events/event.go
@@ -1,3 +1,22 @@
 package events
 
 var Event map[EventId]Mapping
+
+func init() {
+	Event = make(map[EventId]Mapping)
+	Event[Inventory] = Mapping{"Inventory", OnInventory}
+	Event[ToggleFullscreen] = Mapping{"Toggle Fullscreen", OnToggleFullscreen}
+}
+
+type EventId int
+const (
+	nothing EventId = iota
+	Inventory
+	ToggleFullscreen
+)
+
+var (
+	onNothing func(string, string, event interface{})
+	OnInventory
+	OnToggleFullscreen
+)
