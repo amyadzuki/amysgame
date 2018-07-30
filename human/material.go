337ce527c3da1351ee0cecfc5e6d02dc0ab85137
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-30 09:23:56 +0000

    Create material.go

---
+++ human/material.go
@@ -0,0 +1,24 @@
+package human
+
+import (
+	"unsafe"
+
+	"github.com/g3n/engine/gls"
+	"github.com/g3n/engine/material"
+)
+
+type HumanSkinMaterial struct {
+	material.StandardMaterial
+	uni gls.Uniform
+	Udata HumanSkinMaterialUdata
+}
+
+type HumanSkinMaterialUdata struct {
+	Dummy float32
+}
+
+func (m *HumanSkinMaterial) RenderSetup(gs *gls.GLS) {
+	m.StandardMaterial.RenderSetup(gs)
+	location := m.uni.Location(gs)
+	gs.Uniform3fvUP(location, unsafe.Sizeof(m.udata) / 16, unsafe.Pointer(&m.udata))
+}
