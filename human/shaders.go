35a8b6d184cc58cd884fe6c583bee39df8c661b1
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-30 10:28:19 +0000

    Update shaders.go

--- human/shaders.go
+++ human/shaders.go
@@ -19,12 +19,16 @@ var HumanSkinFs = `
 in vec2 vTexcoord;
 out vec4 fColor;
 void main() {
+	vec4 transpSkin;
+	vec3 skin;
+	transpSkin = texture(MatTexture[0], vTexcoord);
+	skin = mix(MatAmbientColor, transpSkin.rgb, transpSkin.a)
 #if MAT_TEXTURES>0
-	fColor = texture(MatTexture[0], vTexcoord);
+	fColor = vec4(skin.rgb, 1)
 #if MAT_TEXTURES>1
 	vec4 uwfc, uw;
 	uwfc = texture(MatTexture[1], vTexcoord);
-	uw = mix(vec4(0, 0, 0, 0), vec4(1, 1, 1, 1), uwfc.r);
+	uw = mix(fColor, vec4(1, 1, 1, 1), uwfc.r);
 	uw = mix(uw, vec4(0.875, 0.875, 0.875, 0.5), uwfc.g);
 	uw = mix(uw, vec4(0xff/255.0, 0xb6/255.0, 0xc1/255.0, 1), uwfc.b);
 	fColor = mix(fColor, uw, uwfc.a);
@@ -32,5 +36,6 @@ void main() {
 #else
 	fColor = vec4(1, 0, 1, 1);
 #endif
+	fColor.a = 1;
 }
 `
