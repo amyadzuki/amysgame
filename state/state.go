cda86f242e821a91e08079e0cd5b8e6d4f813a6e
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-08-01 11:13:49 +0000

    Update state.go

--- state/state.go
+++ state/state.go
@@ -1,7 +1,51 @@
 package state
 
-type State interface {
+type IState interface {
+	Common() *State
 	Enter()
 	Leave()
-	Run()
+	Main()
+}
+
+func (s IState) Run() {
+	common := s.Common()
+	common.current = common.next
+	s.Enter()
+	for !common.win.ShouldClose() && common.next == common.current {
+		s.Main()
+	}
+	s.Leave()
+}
+
+func (s IState) SetNext(next int) {
+	s.Common().next = next
+}
+
+func (s IState) SetWindow(win *window.Window) {
+	s.Common().win = win
+}
+
+type State struct {
+	win     *window.Window
+	current int32
+	next    int32
+}
+
+func (s *State) Common() *State {
+	return s
+}
+
+func (s *State) Enter() {
+}
+
+func (s *State) Init(win *window.Window, state int) {
+	s.win = win
+	s.current = int32(state)
+	s.next = int32(state)
+}
+
+func (s *State) Leave() {
+}
+
+func (s *State) Main() {
 }
