f9c9db166182b005dbd2ab17600faa60132b5a2f
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-08-06 07:31:12 +0000

    Update builder.go

--- human/builder.go
+++ human/builder.go
@@ -9,80 +9,80 @@ import (
 	"github.com/g3n/engine/math32"
 )
 
-type Builder struct {
+type Human struct {
 	age, gender, muscle, weight float64
 
-	*HumanEmbed
+	*Embed
 
 	sync.Mutex
 
 	finalized bool
 }
 
-func New(dec *obj.Decoder) (b *Builder, err error) {
-	b = new(Builder)
-	err = b.Init(dec)
+func New(dec *obj.Decoder) (h *Human, err error) {
+	h = new(Human)
+	err = h.Init(dec)
 	return
 }
 
-func (b *Builder) Finalize() *Builder {
-	b.Lock() ; defer b.Unlock()
-	if !b.finalized {
-		b.update_unlocked(true)
-		b.finalized = true
+func (h *Human) Finalize() *Human {
+	h.Lock() ; defer h.Unlock()
+	if !h.finalized {
+		h.update_unlocked(true)
+		h.finalized = true
 	}
-	return b
+	return h
 }
 
-func (b *Builder) Finalized() bool {
-	return b.finalized
+func (h *Human) Finalized() bool {
+	return h.finalized
 }
 
-func (b *Builder) Init(dec *obj.Decoder) (err error) {
-	b.Lock() ; defer b.Unlock()
+func (h *Human) Init(dec *obj.Decoder) (err error) {
+	h.Lock() ; defer h.Unlock()
 	skin := &math32.Vector4{0.5, 0.5, 0.5, 0.25}
 	eyes := &math32.Color4{1.0/3.0, 2.0/3.0, 1, 1}
 	uwF := &math32.Color4{1, 1, 1, 1}
 	uwD := &math32.Color4{0.875, 0.875, 0.875, 0.5}
 	uwT := &math32.Color4{0xff/255.0, 0xb6/255.0, 0xc1/255.0, 1}
-	if b.human, err = NewHumanEmbed(dec, SkinDark, SkinLight, skin, Eyes, eyes, Underwear, uwF, uwD, uwT); err != nil {
+	if h.Embed, err = NewEmbed(dec, SkinDark, SkinLight, skin, Eyes, eyes, Underwear, uwF, uwD, uwT); err != nil {
 		return
 	}
-	b.age, b.gender, b.muscle, b.weight = 0.5, 0.125, 0.5, 0.5
-	b.finalized = false
-	if BuilderInit != nil {
-		BuilderInit(b)
+	h.age, h.gender, h.muscle, h.weight = 0.5, 0.125, 0.5, 0.5
+	h.finalized = false
+	if HumanInit != nil {
+		HumanInit(h)
 	}
-	if BuilderUpdate != nil {
-		BuilderUpdate(b, false)
+	if HumanUpdate != nil {
+		HumanUpdate(h, false)
 	}
 	return
 }
 
-func (b *Builder) Params() (float64, float64, float64, float64) {
-	return b.age, b.gender, b.muscle, b.weight
+func (h *Human) Params() (float64, float64, float64, float64) {
+	return h.age, h.gender, h.muscle, h.weight
 }
 
-func (b *Builder) Update(age, gender, muscle, weight float64) *Builder {
-	b.Lock() ; defer b.Unlock()
-	if !b.finalized {
-		b.age = maths.ClampFloat64(age, 0, 1)
-		b.gender = maths.ClampFloat64(gender, 0, 1)
-		b.muscle = maths.ClampFloat64(muscle, 0, 1)
-		b.weight = maths.ClampFloat64(weight, 0, 1)
+func (h *Human) Update(age, gender, muscle, weight float64) *Human {
+	h.Lock() ; defer h.Unlock()
+	if !h.finalized {
+		h.age = maths.ClampFloat64(age, 0, 1)
+		h.gender = maths.ClampFloat64(gender, 0, 1)
+		h.muscle = maths.ClampFloat64(muscle, 0, 1)
+		h.weight = maths.ClampFloat64(weight, 0, 1)
 	}
-	b.update_unlocked(false)
-	return b
+	h.update_unlocked(false)
+	return h
 }
 
-func (b *Builder) update_unlocked(final bool) {
-	if b.finalized {
+func (h *Human) update_unlocked(final bool) {
+	if h.finalized {
 		return
 	}
-	if BuilderUpdate != nil {
-		BuilderUpdate(b, final)
+	if HumanUpdate != nil {
+		HumanUpdate(h, final)
 	}
 }
 
-var BuilderInit func(*Builder)
-var BuilderUpdate func(*Builder, bool)
+var HumanInit func(*Human)
+var HumanUpdate func(*Human, bool)
