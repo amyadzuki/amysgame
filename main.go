c1da5de93c10f5b0f1eed7a994bb969ce03c2fd4
amyadzuki <amyadzuki@gmail.com>
2018-08-07 10:24:57 +0000

    Create main.go

---
+++ main.go
@@ -0,0 +1,10 @@
+// COPYRIGHT © 2018 amyadzuki <amyadzuki@gmail.com> ALL RIGHTS RESERVED.
+package main
+
+import "github.com/amyadzuki/amysgame/cmd"
+
+func main() {
+	cmd.Execute()
+}
+
+// COPYRIGHT © 2018 amyadzuki <amyadzuki@gmail.com> ALL RIGHTS RESERVED.
