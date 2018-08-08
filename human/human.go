519d1254572faa694e3123781b4bfc53e9e6303f
amyadzuki <amyadzuki@gmail.com>
2018-08-08 02:26:28 +0000

    Update human.go

--- human/human.go
+++ human/human.go
@@ -153,107 +153,10 @@ func (h *Human) update_unlocked(final bool) {
 	}
 }
 
+var Backest = false
+var HalfEyeHeight float64 = 0.013799965381622314
 var HumanInit func(*Human)
 var HumanUpdate func(*Human, bool)
-
-/*
-func (human *Embed) Init(
-	dec       *obj.Decoder,
-	skinDark  *texture.Texture2D,
-	skinLight *texture.Texture2D,
-	skinDelta *math32.Vector4,
-	eyes      *texture.Texture2D,
-	eyeColor  *math32.Color4,
-	underwear *texture.Texture2D,
-	uwFabric  *math32.Color4,
-	uwDetail  *math32.Color4,
-	uwTrim    *math32.Color4,
-) (err error) {
-	human.Node = core.NewNode()
-	for idx := 0; idx < len(dec.Objects); idx++ {
-		var vbo *gls.VBO
-		var mesh *graphic.Mesh
-		name := dec.Objects[idx].Name
-		switch name {
-		case "high-poly.obj":
-			var m *EyesMaterial
-			vbo, m, mesh, err = NewMeshEyes(dec, eyes, eyeColor, &dec.Objects[idx])
-			if vbo == nil {
-				panic("VboEyes was nil")
-			}
-			human.VboEyes, human.MatEyes, human.Eyes = vbo, m, mesh
-			_, highest, frontest, ok := ofsRange(dec, &dec.Objects[idx])
-			if ! ok {
-				fmt.Printf("No vertices in \"%s\"\n", name)
-			}
-			human.heightToEye = float64(highest) - HalfEyeHeight
-			human.frontOfEye = float64(frontest)
-		case "female_generic.obj":
-			fallthrough
-		case "male_generic.obj":
-			var m *SkinMaterial
-			vbo, m, mesh, err = NewMeshSkin(dec, skinDark, skinLight, skinDelta,
-				underwear, uwFabric, uwDetail, uwTrim, &dec.Objects[idx])
-			if vbo == nil {
-				panic("VboSkin was nil")
-			}
-			human.VboSkin, human.MatSkin, human.Skin = vbo, m, mesh
-			lowest, highest, _, ok := ofsRange(dec, &dec.Objects[idx])
-			if ! ok {
-				fmt.Printf("No vertices in \"%s\"\n", name)
-			}
-			human.base = float64(lowest)
-			human.heightToCap = float64(highest) - human.base
-		default:
-			fmt.Printf("Skipping: \"%s\"\n", name)
-			continue;
-		}
-		if err != nil {
-			return
-		}
-		human.Node.Add(mesh)
-	}
-	return nil
-}
-
-func ofsRange(dec *obj.Decoder, object *obj.Object) (lowest, highest, frontest float32, ok bool) {
-	for _, face := range object.Faces {
-		for _, vertex := range face.Vertices {
-			y := dec.Vertices[vertex + 1]
-			z := dec.Vertices[vertex + 2]
-			if !false || z >= 1 {
-				if ok {
-					if z < lowest {
-						lowest = z
-					}
-					if z > highest {
-						highest = z
-					}
-					if !Backest {
-						if y < frontest {
-							frontest = y
-						}
-					} else {
-						if y > frontest {
-							frontest = y
-						}
-					}
-				} else {
-					lowest = z
-					highest = z
-					frontest = y
-					ok = true
-				}
-			}
-		}
-	}
-	// fmt.Printf("\"%s\": %f v^ %f (%v)  |  %f\n", object.Name, lowest, highest, 0.5 * (float64(highest) - float64(lowest)), frontest)
-	return
-}
-*/
-
-var HalfEyeHeight float64 = 0.013799965381622314
-var Backest = false
 var VboUvs *gls.VBO
 
 func init() {
