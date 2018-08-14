8b20bbefe94612a788e5bb1c4c6d197358a3121c
Amy <amyadzuki@gmail.com>
2018-08-14 10:25:42 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,50 @@
 // COPYRIGHT © 2018 amyadzuki <amyadzuki@gmail.com> ALL RIGHTS RESERVED.
 package main
 
-import "github.com/amyadzuki/amysgame/cobra"
+import (
+	"github.com/amyadzuki/amysgame/run"
+
+	"github.com/amy911/env911"
+	"github.com/amy911/env911/app"
+	"github.com/amy911/env911/config"
+)
+
+func init() {
+	env911.InitAll("AMYSGAME_", f, "amyadzuki", "amysgame") // TODO: better vendor and app names!!
+}
 
 func main() {
-	cobra.Execute()
+	copyright := config.Bool("copyright", false, "Print the copyright notice and exit")
+	eula := config.Bool("eula", false, "Print the End User License Agreement (EULA) and exit")
+	legal := config.Bool("legal", false, "Print the copyright notice and exit")
+	license := config.Bool("license", false, "Print the End User License Agreement (EULA) and exit")
+	version := config.Bool("version", false, "Print the version number and exit")
+	quick := config.Bool("quick", false, "Skip the launcher and just play the game played previously")
+
+	config.BoolVarP(&vars.Verbose, "verbose", "v", false, "Write more output")
+
+	config.BoolVar(&vars.Debug, "debug", false, "Write more log output")
+	config.BoolVar(&vars.Trace, "debugextra", false, "Write significantly more log output")
+
+	config.BoolVar(&vars.Json, "json", false, "Use JSON for the data format")
+	config.BoolVar(&vars.Xml, "xml", false, "Use XML for the data format")
+	config.BoolVar(&vars.Yaml, "yaml", false, "Use YAML for the data format")
+
+	config.LoadAndParse()
+
+	want_copyright := *legal || *copyright
+	want_license := *legal || *eula || *license
+	switch {
+	case want_copyright && want_license:
+	case want_license:
+	case want_copyright:
+	case *version:
+	default:
+		if quick {
+			// TODO
+		}
+		run.Play()
+	}
 }
 
 // COPYRIGHT © 2018 amyadzuki <amyadzuki@gmail.com> ALL RIGHTS RESERVED.
